887d2b092acb63e7c3e571ce0f1e92ddc8217e21
zogreptile <zogreptile@gmail.com>
2018-09-04 19:09:02 +1000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# RSS Reader
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/zogreptile/project-lvl3-s306/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/zogreptile/project-lvl3-s306/test_coverage)
+[![Build Status](https://travis-ci.org/zogreptile/project-lvl3-s306.svg?branch=master)](https://travis-ci.org/zogreptile/project-lvl3-s306)
+
+## Setup
+
+```sh
+make install
+```
+
+### Run
+
+```sh
+make develop
+```
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
